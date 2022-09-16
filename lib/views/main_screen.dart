53ca95d863a991950b8f11b83e745e53b4c15895
Stephen Essoun <boit7845@gmail.com>
2022-09-16 22:44:07 +0000

    connected to firebase and added bottom nav bar

--- lib/views/main_screen.dart
+++ lib/views/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:pillset/commons/utils/colors.dart';
 import 'package:pillset/views/nav_view/fourth_view.dart';
 import 'package:pillset/views/nav_view/home_view.dart';
@@ -20,30 +21,66 @@ class _MainScreenState extends State<MainScreen> {
     StatisticsView(),
     ProfileView()
   ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: pages[currentIndex],
-        bottomNavigationBar: BottomAppBar(
-      child: BottomNavigationBar(
-        selectedItemColor: green,
-        unselectedItemColor: darkGrey,
-        onTap: ((index) {
-          setState(() {
-          index = currentIndex;
-            
-          });
-        }),
-        items:const [
-        BottomNavigationBarItem(
-            icon: Icon(Icons.access_time_outlined), label: ''),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.access_time_outlined), label: ''),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.access_time_outlined), label: ''),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.access_time_outlined), label: ''),
-      ]),
-    ));
+    OnTap onTap = OnTap();
+    return ValueListenableBuilder(
+       valueListenable: onTap,
+      builder: (context,value,child) {
+        return Scaffold(
+          // extendBody: true,
+          body: SafeArea(
+            bottom: false,
+            child: pages[value],
+          ),
+          bottomNavigationBar: BottomAppBar(
+            notchMargin: 12,
+            color: white,
+            shape: const CircularNotchedRectangle(),
+            child: BottomNavigationBar(
+                // key: navKey,
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                type: BottomNavigationBarType.fixed,
+                selectedItemColor: green,
+                unselectedItemColor: whiteGrey,
+                currentIndex: value,
+                selectedFontSize: 0,
+                unselectedFontSize: 0,
+                onTap: (index) =>
+                    onTap.whenTapped(index),
+                items: const [
+                  BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: ''),
+                  BottomNavigationBarItem(
+                      icon: FaIcon(FontAwesomeIcons.fire), label: ''),
+                  BottomNavigationBarItem(
+                      icon: FaIcon(FontAwesomeIcons.chartSimple), label: ''),
+                  BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
+                ]),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: darkGrey,
+            foregroundColor: white,
+            child: const FaIcon(
+              FontAwesomeIcons.plus,
+              size: 35,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class OnTap extends ValueNotifier<int> {
+  OnTap._sharedInference() : super(0);
+  static final OnTap _shared = OnTap._sharedInference();
+  factory OnTap() => _shared;
+  void whenTapped(int index) {
+    value = index;
+    notifyListeners();
   }
 }
